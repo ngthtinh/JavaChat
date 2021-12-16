5dd2f339e0bd18165b2b00311fbd087fc9cef012
ngthtinh <nguyenthanhtinh365@gmail.com>
2021-12-16 23:35:00 +0700

    Tạo Java Projects, thêm Đề bài

---
+++ JavaChatServer/src/vn/edu/hcmus/student/_19127292/JavaChatServer/Main.java
@@ -0,0 +1,13 @@
+package vn.edu.hcmus.student._19127292.JavaChatServer;
+
+/**
+ * vn.edu.hcmus.student._19127292.JavaChatServer
+ * Created by 19127292 - Nguyen Thanh Tinh
+ * Date 16-Dec-21 - 23:29
+ * Description: Main Class
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
