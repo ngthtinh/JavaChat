016a5db307acc32ee2febd6d505f509be493fd5f
ngthtinh <nguyenthanhtinh365@gmail.com>
2021-12-18 23:42:17 +0700

    Đăng ký, Màn hình chính: Xong

--- JavaChatClient/src/vn/edu/hcmus/student/_19127292/JavaChatClient/Main.java
+++ JavaChatClient/src/vn/edu/hcmus/student/_19127292/JavaChatClient/Main.java
@@ -1,13 +1,98 @@
 package vn.edu.hcmus.student._19127292.JavaChatClient;
 
+import javax.swing.*;
+import java.awt.*;
+
+import javax.swing.border.EmptyBorder;
+
 /**
  * vn.edu.hcmus.student._19127292.JavaChatClient
  * Created by 19127292 - Nguyen Thanh Tinh
  * Date 16-Dec-21 - 23:26
  * Description: Main Class
  */
-public class Main {
+public class Main extends JFrame {
+    JLabel conversationTitle;
+
     public static void main(String[] args) {
         new SignIn();
+    }
+
+    public Main() {
+        addComponents();
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setTitle("Java Chat App");
+        setVisible(true);
+    }
+
+    public void addComponents() {
+        // Content Pane
+        JPanel contentPane = new JPanel();
+        contentPane.setLayout(new BorderLayout());
+
+        // Left Panel
+        JPanel leftPanel = new JPanel();
+        leftPanel.setLayout(new BorderLayout());
+        leftPanel.setBorder(BorderFactory.createMatteBorder(0, 0, 0, 1, Color.LIGHT_GRAY));
+
+        JLabel onlineTitle = new JLabel("Online users       ");
+        onlineTitle.setFont(new Font("Arial", Font.BOLD, 20));
+
+        String[] user = {"A", "B"};
+        JList<String> userList = new JList<>(user);
+        userList.addListSelectionListener(e -> changeConversation(userList.getSelectedValue()));
+
+        JScrollPane userScroll = new JScrollPane(userList);
+        userScroll.setBorder(new EmptyBorder(10, 0, 0, 0));
+
+        JPanel userPanel = new JPanel();
+        userPanel.setLayout(new BorderLayout());
+        userPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
+        userPanel.add(onlineTitle, BorderLayout.PAGE_START);
+        userPanel.add(userScroll, BorderLayout.CENTER);
+
+        leftPanel.add(userPanel);
+
+        // Right Panel
+        JPanel rightPanel = new JPanel();
+        rightPanel.setLayout(new BorderLayout());
+        rightPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
+
+        conversationTitle = new JLabel("");
+        conversationTitle.setFont(new Font("Arial", Font.BOLD, 20));
+
+        JTextArea conversationTextArea = new JTextArea();
+        JScrollPane messageScroll = new JScrollPane(conversationTextArea);
+        messageScroll.setBorder(new EmptyBorder(10, 0, 10, 0));
+
+        JTextField messageTextField = new JTextField(20);
+        JButton sendButton = new JButton("Send");
+        sendButton.addActionListener(e -> sendButtonEventListener());
+
+        JPanel messagePanel = new JPanel();
+        messagePanel.setLayout(new BoxLayout(messagePanel, BoxLayout.X_AXIS));
+        messagePanel.add(messageTextField);
+        messagePanel.add(Box.createHorizontalStrut(5));
+        messagePanel.add(sendButton);
+
+        rightPanel.add(conversationTitle, BorderLayout.PAGE_START);
+        rightPanel.add(messageScroll, BorderLayout.CENTER);
+        rightPanel.add(messagePanel, BorderLayout.PAGE_END);
+
+        // Add components to content pane and Settings
+        contentPane.add(leftPanel, BorderLayout.LINE_START);
+        contentPane.add(rightPanel, BorderLayout.CENTER);
+
+        setContentPane(contentPane);
+        pack();
+    }
+
+    void sendButtonEventListener() {
+
+    }
+
+    void changeConversation(String conversationUser) {
+        conversationTitle.setText(conversationUser);
     }
 }
--- JavaChatClient/src/vn/edu/hcmus/student/_19127292/JavaChatClient/SignUp.java
+++ JavaChatClient/src/vn/edu/hcmus/student/_19127292/JavaChatClient/SignUp.java
@@ -8,15 +8,15 @@ import javax.swing.border.EmptyBorder;
 /**
  * vn.edu.hcmus.student._19127292.JavaChatClient
  * Created by 19127292 - Nguyen Thanh Tinh
- * Date 17-Dec-21 - 18:50
- * Description: Sign In Frame
+ * Date 18-Dec-21 - 21:19
+ * Description: Sign Up Frame
  */
-public class SignIn extends JFrame {
-    public SignIn() {
+public class SignUp extends JFrame {
+    public SignUp() {
         addComponents();
 
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setTitle("Sign In");
+        setTitle("Sign Up Now");
         setResizable(false);
         setVisible(true);
     }
@@ -27,9 +27,17 @@ public class SignIn extends JFrame {
         contentPane.setLayout(new GridLayout(6, 1, 0, 5));
         contentPane.setBorder(new EmptyBorder(10, 10, 10, 10));
 
+        // Back to Sign In Button
+        JButton backButton = new JButton("Back to Sign In");
+        backButton.addActionListener(e -> backButtonEventHandler());
+
+        JPanel backPanel = new JPanel();
+        backPanel.setLayout(new BoxLayout(backPanel, BoxLayout.X_AXIS));
+        backPanel.add(backButton);
+
         // Title Label
-        JLabel titleLabel = new JLabel("JAVA CHAT APP", JLabel.CENTER);
-        titleLabel.setFont(new Font("Arial", Font.BOLD, 20));
+        JLabel titleLabel = new JLabel("SIGN UP NOW", JLabel.CENTER);
+        titleLabel.setFont(new Font("Arial", Font.BOLD, 15));
 
         // Username Panel
         JTextField usernameTextField = new JTextField(15);
@@ -37,7 +45,7 @@ public class SignIn extends JFrame {
         JPanel usernamePanel = new JPanel();
         usernamePanel.setLayout(new BoxLayout(usernamePanel, BoxLayout.X_AXIS));
         usernamePanel.add(new JLabel("Username"));
-        usernamePanel.add(Box.createHorizontalStrut(10));
+        usernamePanel.add(Box.createHorizontalStrut(40));
         usernamePanel.add(usernameTextField);
 
         // Password Panel
@@ -46,36 +54,41 @@ public class SignIn extends JFrame {
         JPanel passwordPanel = new JPanel();
         passwordPanel.setLayout(new BoxLayout(passwordPanel, BoxLayout.X_AXIS));
         passwordPanel.add(new JLabel("Password"));
-        passwordPanel.add(Box.createHorizontalStrut(11));
+        passwordPanel.add(Box.createHorizontalStrut(41));
         passwordPanel.add(passwordTextField);
 
-        // Sign in Button
-        JButton signInButton = new JButton("SIGN IN");
-        signInButton.addActionListener(e -> signInButtonEventHandler());
+        // Password Panel
+        JTextField repasswordTextField = new JTextField(15);
 
-        // Sign up now Button
+        JPanel repasswordPanel = new JPanel();
+        repasswordPanel.setLayout(new BoxLayout(repasswordPanel, BoxLayout.X_AXIS));
+        repasswordPanel.add(new JLabel("Re-password"));
+        repasswordPanel.add(Box.createHorizontalStrut(23));
+        repasswordPanel.add(repasswordTextField);
+
+        // Sign Up Now Button
         JButton signUpNowButton = new JButton("SIGN UP NOW");
         signUpNowButton.addActionListener(e -> signUpNowButtonEventHandler());
 
         // Add components to Content Pane and Settings
+        contentPane.add(backPanel);
         contentPane.add(titleLabel);
         contentPane.add(usernamePanel);
         contentPane.add(passwordPanel);
-        contentPane.add(signInButton);
-        contentPane.add(new JLabel("or", JLabel.CENTER));
+        contentPane.add(repasswordPanel);
         contentPane.add(signUpNowButton);
 
         setContentPane(contentPane);
         pack();
     }
 
-    void signInButtonEventHandler() {
-        new Main();
+    void backButtonEventHandler() {
+        new SignIn();
         dispose();
     }
 
     void signUpNowButtonEventHandler() {
-        new SignUp();
+        new Main();
         dispose();
     }
 }
